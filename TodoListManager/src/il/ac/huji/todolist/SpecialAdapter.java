65793b5449af712dd092e63e3f501cfa1bc59af1
kgnvch <kgnvch@gmail.com>
2013-03-24 14:28:32 +0200

    ex3

--- TodoListManager/src/il/ac/huji/todolist/SpecialAdapter.java
+++ TodoListManager/src/il/ac/huji/todolist/SpecialAdapter.java
@@ -3,16 +3,12 @@ package il.ac.huji.todolist;
 import java.util.List;
 import android.content.Context;
 import android.graphics.Color;
-<<<<<<< HEAD
 import android.view.LayoutInflater;
-=======
->>>>>>> 39ec1c0cf691d8cff2eb1b0e81c6169448943aa0
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-<<<<<<< HEAD
 public class SpecialAdapter extends ArrayAdapter<Task> {
 
 	public SpecialAdapter(Context context, int textViewResourceId, List<Task> objects) {
@@ -41,22 +37,5 @@ public class SpecialAdapter extends ArrayAdapter<Task> {
 
 		return view;
 	}
-=======
-public class SpecialAdapter extends ArrayAdapter<String> {
-     
-    public SpecialAdapter(Context context, int textViewResourceId, List<String> objects) {
-        super(context, textViewResourceId, objects);
-    }
- 
-    /**
-     * Set the row text colors to red/blue alternately.
-     */
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-      View view = super.getView(position, convertView, parent);
-      ((TextView)view).setTextColor(position % 2 == 0 ? Color.RED : Color.BLUE);
-      return view;
-    }
->>>>>>> 39ec1c0cf691d8cff2eb1b0e81c6169448943aa0
 
 }
